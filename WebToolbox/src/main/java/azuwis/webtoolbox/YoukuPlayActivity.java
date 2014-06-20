ad051b34370354096fa0f4f912c543cf86fc20f1
Zhong Jianxin <azuwis@gmail.com>
2014-06-20 17:51:16 +0800

    Youku m3u8 url update

--- WebToolbox/src/main/java/azuwis/webtoolbox/YoukuPlayActivity.java
+++ WebToolbox/src/main/java/azuwis/webtoolbox/YoukuPlayActivity.java
@@ -10,7 +10,7 @@ public abstract class YoukuPlayActivity extends BasePlayActivity {
         Pattern pattern = Pattern.compile(".*/id_([A-Za-z0-9]+).*");
         Matcher matcher = pattern.matcher(url);
         if (matcher.matches()) {
-            return String.format("http://v.youku.com/player/getRealM3U8/vid/%s/type/%s/v.m3u8",
+            return String.format("http://v.youku.com/player/getM3U8/vid/%s/type/%s/video.m3u8",
                     matcher.group(1), getType());
         } else {
             return null;
